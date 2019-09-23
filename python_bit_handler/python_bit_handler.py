c305f8d485e669842f29942d48bc3aa789e1b743
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-09-23 22:31:26 +0300

    Add get_number from raw bytes.

--- python_bit_handler/python_bit_handler.py
+++ python_bit_handler/python_bit_handler.py
@@ -7,7 +7,7 @@ class BitHandler():
     """A module for handling bits in/from registers.
 
     It's purpose is to be used with some hardware communication library like
-    spidev2 or smbus and abstract the handling of various bits from registers.
+    spidev or smbus2 and abstract the handling of various bits from registers.
 
     Args:
         resolution: The resolution of the registers in bits.
@@ -17,7 +17,7 @@ class BitHandler():
         """Constructor."""
 
         self._resolution = resolution
-
+    
     def set_bits(self, register_val, value, num_bits, shift):
         """Set shift bits from start of register with value.
 
@@ -47,3 +47,50 @@ class BitHandler():
         mask = ((1 << num_bits) - 1) << shift
 
         return (register_val & mask) >> shift
+
+    def get_number(self, data, res, signed=False, rev=False):
+        """From a list with raw bytes make a number.
+
+        For example data is [10100101, 11110000, 10100000] and the bits we want
+        are the first 16 and just the first three of the last one. The result
+        would be 1010010111110000101.
+
+        Args:
+            data: A list with the raw bytes.
+            res: The bit resolution of the target number.
+            signed: If the number is signed must be True.
+            rev: If the msb is in the first entry must be True.
+
+        Return:
+            An integer of resolution res.
+        """
+
+        # Raise exception if data list has more entries than the needed.
+        if len(data)*self._resolution - res >= self._resolution:
+            raise Exception("No need for last entry of data.")
+
+        # Make it a list if it is one element
+        data = data if isinstance(data, list) else [data]
+        
+        # Reverse it
+        if rev:
+            data.reverse()
+
+        # Find the bit number of the lsb
+        low_res = res % self._resolution
+        low_res = low_res if low_res else self._resolution
+
+        # Get only the bits of interest
+        data[0] = self.get_bits(data[0], low_res, self._resolution - low_res)
+
+        # Compute the whole number from spare bytes
+        num = data[0]
+        for (i, d) in enumerate(data[1:], start=1):
+            num += d << (i*self._resolution - (self._resolution-low_res))
+
+        power = res - 1
+        whole = 2**(power+1) - 1
+        if signed:
+            num = num if num < (2**power - 1) else -((whole ^ num) + 1)
+
+        return num
--- tests/test_python_bit_handler.py
+++ tests/test_python_bit_handler.py
@@ -5,6 +5,7 @@
 
 
 import unittest
+import ctypes
 from click.testing import CliRunner
 
 from python_bit_handler import BitHandler
@@ -51,6 +52,23 @@ class TestPython_bit_handler(unittest.TestCase):
                 print("Bits: {}, Shift: {}, Value: {}".format(j, 
                                                               i,
                                                               bin(new_reg_val)))
+    
+    def test_number(self):
+        """Test get_number function."""
+
+        handler = BitHandler(resolution=8)
+        data = [0b10010101, 0b00100111, 0b11010000]
+        whole_res = 0b100101010010011111010000
+
+        res = 24
+
+        for i in range(8):
+            s = False
+            number = handler.get_number(data.copy(), res=res, signed=s, rev=True)
+            self.assertEqual(number, 
+                             handler.get_bits(whole_res, res, i),
+                             "Wrong at res {} and signed {}".format(res, s))
+            res -= 1
 
     def test_command_line_interface(self):
         """Test the CLI."""
